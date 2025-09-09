a221efae9e7bba54a605677c9eb28574bb405d00
rdos314 <leif@rdos.net>
2025-09-09 22:38:20 +0200

    Add all source as Vivado fails to build (wrapper not found).

---
+++ vivado/adc.gen/sources_1/bd/ps/hdl/ps_wrapper.v
@@ -0,0 +1,19 @@
+//Copyright 1986-2022 Xilinx, Inc. All Rights Reserved.
+//Copyright 2022-2025 Advanced Micro Devices, Inc. All Rights Reserved.
+//--------------------------------------------------------------------------------
+//Tool Version: Vivado v.2025.1 (win64) Build 6140274 Thu May 22 00:12:29 MDT 2025
+//Date        : Tue Sep  9 21:17:40 2025
+//Host        : DESKTOP-SA3FM6F running 64-bit major release  (build 9200)
+//Command     : generate_target ps_wrapper.bd
+//Design      : ps_wrapper
+//Purpose     : IP block netlist
+//--------------------------------------------------------------------------------
+`timescale 1 ps / 1 ps
+
+module ps_wrapper
+   ();
+
+
+  ps ps_i
+       ();
+endmodule
