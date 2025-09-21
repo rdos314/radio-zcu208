6b3a933fcc94ba2c58153016ee2c2f566f6dfafd
rdos314 <leif@rdos.net>
2025-09-21 13:43:56 +0200

    Add dummy app

--- _ide/workspace_journal.py
+++ _ide/workspace_journal.py
@@ -1,8 +1,16 @@
-# 2025-09-20T23:53:46.813194700
+# 2025-09-21T13:37:57.742243
 import vitis
 
 client = vitis.create_client()
 client.set_workspace(path="C:/radio-zcu208")
+
+comp = client.create_app_component(name="dummy",platform = "$COMPONENT_LOCATION/../platform/export/platform/platform.xpfm",domain = "standalone_psu_cortexr5_0")
+
+platform = client.get_component(name="platform")
+status = platform.build()
+
+comp = client.get_component(name="dummy")
+comp.build()
 
 vitis.dispose()
 
---
+++ _ide/workspace_journal_9052.py
@@ -0,0 +1,8 @@
+# 2025-09-20T23:53:46.813194700
+import vitis
+
+client = vitis.create_client()
+client.set_workspace(path="C:/radio-zcu208")
+
+vitis.dispose()
+
