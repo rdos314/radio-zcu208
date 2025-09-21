021b250f9c9fe82a7a08e006aacb9325215ec9b7
rdos314 <leif@rdos.net>
2025-09-21 16:05:41 +0200

    Removed PMU and FSBL from platform.

--- _ide/workspace_journal.py
+++ _ide/workspace_journal.py
@@ -1,8 +1,26 @@
-# 2025-09-21T15:06:24.977389300
+# 2025-09-21T16:02:59.138199400
 import vitis
 
 client = vitis.create_client()
 client.set_workspace(path="C:/radio-zcu208")
 
-vitis.dispose()
+platform = client.get_component(name="platform")
+status = platform.build()
+
+comp = client.get_component(name="analyse")
+comp.build()
+
+status = platform.build()
+
+comp = client.get_component(name="control")
+comp.build()
+
+status = platform.build()
+
+comp = client.get_component(name="dummy")
+comp.build()
+
+status = platform.remove_boot_bsp()
+
+status = platform.build()
 
--- platform/zynqmp_fsbl/zynqmp_fsbl_bsp/include/DRVLISTConfig.cmake
+++
@@ -1,1 +0,0 @@
-set(DRIVER_LIST axipmon;clockps;common;coresightps_dcc;csudma;ddrcpsu;emacps;gpio;gpiops;iicps;ipipsu;qspipsu;resetps;rtcpsu;scugic;sdps;sysmonpsu;tmrctr;ttcps;uartps;usbpsu;wdtps;zdma)
