c619880af0e8c87aaabc6ac85018ed127cda3d31
Leif Ekblad <leif@rdos.net>
2025-09-22 09:01:51 +0200

    Removed non-needed files.

--- _ide/workspace_journal.py
+++
@@ -1,8 +0,0 @@
-# 2025-09-21T21:04:44.457130500
-import vitis
-
-client = vitis.create_client()
-client.set_workspace(path="C:/radio-zcu208")
-
-vitis.dispose()
-
--- _ide/workspace_journal_11340.py
+++
@@ -1,8 +0,0 @@
-# 2025-09-21T14:50:02.248653200
-import vitis
-
-client = vitis.create_client()
-client.set_workspace(path="C:/radio-zcu208")
-
-vitis.dispose()
-
