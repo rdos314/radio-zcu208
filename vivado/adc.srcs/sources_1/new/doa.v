fba2ee91dd6e7bea3724f7a22f8cdc30e53dd0d9
Leif Ekblad <leif@rdos.net>
2025-10-02 10:03:49 +0200

    Provided the correct MTS SYSREF interface from PG269.

--- vivado/adc.srcs/sources_1/new/doa.v
+++ vivado/adc.srcs/sources_1/new/doa.v
@@ -21,59 +21,67 @@
 
 
 module doa(
-    input clk,
-    input [159:0] data_N,
-    input ready_N,
-    input [159:0] data_E,
-    input ready_E,
-    input [159:0] data_W,
-    input ready_W
+    input wire	clk,
+	input wire	reset,
+    input wire	[159:0] data_N,
+    input wire	ready_N,
+    input wire	[159:0] data_E,
+    input wire	ready_E,
+    input wire	[159:0] data_W,
+    input wire	ready_W
     );
 
-ila_0 ila_N (
-  .clk(clk),                    // input wire clk
-  .probe0(data_N[15:0]),        // input wire [15:0]  probe3
-  .probe1(data_N[31:16]),       // input wire [15:0]  probe3
-  .probe2(data_N[47:32]),       // input wire [15:0]  probe3
-  .probe3(data_N[63:48]),       // input wire [15:0]  probe3
-  .probe4(data_N[79:64]),       // input wire [15:0]  probe3
-  .probe5(data_N[95:80]),       // input wire [15:0]  probe3
-  .probe6(data_N[111:96]),      // input wire [15:0]  probe3
-  .probe7(data_N[127:112]),     // input wire [15:0]  probe3
-  .probe8(data_N[143:128]),     // input wire [15:0]  probe3
-  .probe9(data_N[159:144]),     // input wire [15:0]  probe3
-  .probe10(ready_N)             // input wire [0:0]  probe3
-);
+	wire 	pl_clk;
 
-ila_0 ila_E (
-  .clk(clk),                    // input wire clk
-  .probe0(data_E[15:0]),        // input wire [15:0]  probe3
-  .probe1(data_E[31:16]),       // input wire [15:0]  probe3
-  .probe2(data_E[47:32]),       // input wire [15:0]  probe3
-  .probe3(data_E[63:48]),       // input wire [15:0]  probe3
-  .probe4(data_E[79:64]),       // input wire [15:0]  probe3
-  .probe5(data_E[95:80]),       // input wire [15:0]  probe3
-  .probe6(data_E[111:96]),      // input wire [15:0]  probe3
-  .probe7(data_E[127:112]),     // input wire [15:0]  probe3
-  .probe8(data_E[143:128]),     // input wire [15:0]  probe3
-  .probe9(data_E[159:144]),     // input wire [15:0]  probe3
-  .probe10(ready_E)             // input wire [0:0]  probe3
-);
+	BUFG pl_clk_i (
+		.I			(clk),
+		.O			(pl_clk)
+	);
 
-ila_0 ila_W (
-  .clk(clk),                    // input wire clk
-  .probe0(data_W[15:0]),        // input wire [15:0]  probe3
-  .probe1(data_W[31:16]),       // input wire [15:0]  probe3
-  .probe2(data_W[47:32]),       // input wire [15:0]  probe3
-  .probe3(data_W[63:48]),       // input wire [15:0]  probe3
-  .probe4(data_W[79:64]),       // input wire [15:0]  probe3
-  .probe5(data_W[95:80]),       // input wire [15:0]  probe3
-  .probe6(data_W[111:96]),      // input wire [15:0]  probe3
-  .probe7(data_W[127:112]),     // input wire [15:0]  probe3
-  .probe8(data_W[143:128]),     // input wire [15:0]  probe3
-  .probe9(data_W[159:144]),     // input wire [15:0]  probe3
-  .probe10(ready_W)             // input wire [0:0]  probe3
-);
+	ila_0 ila_N (
+		.clk(pl_clk),                 // input wire clk
+		.probe0(data_N[15:0]),        // input wire [15:0]  probe3
+		.probe1(data_N[31:16]),       // input wire [15:0]  probe3
+		.probe2(data_N[47:32]),       // input wire [15:0]  probe3
+		.probe3(data_N[63:48]),       // input wire [15:0]  probe3
+		.probe4(data_N[79:64]),       // input wire [15:0]  probe3
+		.probe5(data_N[95:80]),       // input wire [15:0]  probe3
+		.probe6(data_N[111:96]),      // input wire [15:0]  probe3
+		.probe7(data_N[127:112]),     // input wire [15:0]  probe3
+		.probe8(data_N[143:128]),     // input wire [15:0]  probe3
+		.probe9(data_N[159:144]),     // input wire [15:0]  probe3
+		.probe10(ready_N)             // input wire [0:0]  probe3
+	);
+
+	ila_0 ila_E (
+		.clk(pl_clk),                 // input wire clk
+		.probe0(data_E[15:0]),        // input wire [15:0]  probe3
+		.probe1(data_E[31:16]),       // input wire [15:0]  probe3
+		.probe2(data_E[47:32]),       // input wire [15:0]  probe3
+		.probe3(data_E[63:48]),       // input wire [15:0]  probe3
+		.probe4(data_E[79:64]),       // input wire [15:0]  probe3
+		.probe5(data_E[95:80]),       // input wire [15:0]  probe3
+		.probe6(data_E[111:96]),      // input wire [15:0]  probe3
+		.probe7(data_E[127:112]),     // input wire [15:0]  probe3
+		.probe8(data_E[143:128]),     // input wire [15:0]  probe3
+		.probe9(data_E[159:144]),     // input wire [15:0]  probe3
+		.probe10(ready_E)             // input wire [0:0]  probe3
+	);
+
+	ila_0 ila_W (
+		.clk(pl_clk),                 // input wire clk
+		.probe0(data_W[15:0]),        // input wire [15:0]  probe3
+		.probe1(data_W[31:16]),       // input wire [15:0]  probe3
+		.probe2(data_W[47:32]),       // input wire [15:0]  probe3
+		.probe3(data_W[63:48]),       // input wire [15:0]  probe3
+		.probe4(data_W[79:64]),       // input wire [15:0]  probe3
+		.probe5(data_W[95:80]),       // input wire [15:0]  probe3
+		.probe6(data_W[111:96]),      // input wire [15:0]  probe3
+		.probe7(data_W[127:112]),     // input wire [15:0]  probe3
+		.probe8(data_W[143:128]),     // input wire [15:0]  probe3
+		.probe9(data_W[159:144]),     // input wire [15:0]  probe3
+		.probe10(ready_W)             // input wire [0:0]  probe3
+	);
     
 generate
   begin : doa
--- vivado/adc.srcs/sources_1/new/mts.v
+++ vivado/adc.srcs/sources_1/new/mts.v
@@ -19,46 +19,59 @@
 // 
 //////////////////////////////////////////////////////////////////////////////////
 
-
 module mts(
-    input wire pl_clk_p,
-    input wire pl_clk_n,
-    
-    (* X_INTERFACE_PARAMETER = "XIL_INTERFACENAME SI_CLK, FREQ_HZ 125000000, FREQ_TOLERANCE_HZ 0, PHASE 0.0" *)
-    output wire pl_clk,
-
-    input wire pl_sysref_p,
-    input wire pl_sysref_n,
-    output wire pl_sysref,
-    output reg user_sysref_adc,
-    input wire m0_axis_aclk
+    input wire 	pl_clk_p,
+    input wire 	pl_clk_n,
+    output wire m_clk,
+	output wire m_clk_buf,
+    output wire reset,    
+    output wire resetn, 
+    input wire	pl_sysref_p,
+    input wire 	pl_sysref_n,
+    output reg 	user_sysref_adc
     );
 
-  reg           pl_sysref_r;
+	reg       	pl_sysref_r;
+	wire		pl_clk;
+	wire		pl_clk_buf;
 
-
+	assign 		reset = !reset_n;
+	
 generate
   begin : mts
         
-    // Multi-tile sync logic
-      IBUFGDS pl_clk_i (
+	IBUFDS pl_clk_i (
         .I          (pl_clk_p),
         .IB         (pl_clk_n),
         .O          (pl_clk));
         
-      IBUFGDS pl_sysref_i (
+	IBUFDS pl_sysref_i (
         .I          (pl_sysref_p),
         .IB         (pl_sysref_n),
         .O          (pl_sysref));
-        
-      always @(posedge pl_clk) begin
-        pl_sysref_r <= pl_sysref;
-      end
-        
-      always @(posedge m0_axis_aclk) begin
-        user_sysref_adc <= pl_sysref_r;
-      end
+		
+	BUFG p_clk_i (
+		.I			(pl_clk),
+		.O			(pl_clk_buf));
+    
+	clk_wiz pl_clk_wiz_i (
+		.clk_in1	(pl_clk_buf),
+		.clk_out1	(m_clk),
+		.locked		(reset_n));
+		
+	BUFG m_clk_i (
+		.I			(m_clk),
+		.O			(m_clk_buf));
 
+	always @(posedge pl_clk_buf) 
+	begin
+		pl_sysref_r <= pl_sysref;
+	end
+        
+	always @(posedge m_clk_buf) 
+	begin
+		user_sysref_adc <= pl_sysref_r;
+	end
 
   end
     
