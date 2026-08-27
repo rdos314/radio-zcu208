744f67c0d82501a732493913a290a09dd175b40a
Leif Ekblad <leif@rdos.net>
2026-08-27 14:42:43 +0200

    Added DAC config to 2 GHz.

--- control/src/adcconf.c
+++ control/src/adcconf.c
@@ -145,6 +145,31 @@ const u32 LMK_CKin[LMK_COUNT] =
 
 #define LMX2594_COUNT 116
 
+const u32 LMX2594G2000[LMX2594_COUNT] =
+{
+	/* LMX2594_REF-250M__2000M.txt */
+	0x000002, 0x000000, 0x700000, 0x6F0000, 0x6E0000, 0x6D0000,
+	0x6C0000, 0x6B0000, 0x6A0000, 0x690021, 0x680000, 0x670000,
+	0x663F80, 0x650011, 0x640000, 0x630000, 0x620200, 0x610888,
+	0x600000, 0x5F0000, 0x5E0000, 0x5D0000, 0x5C0000, 0x5B0000,
+	0x5A0000, 0x590000, 0x580000, 0x570000, 0x560000, 0x55D300,
+	0x540001, 0x530000, 0x521E00, 0x510000, 0x506666, 0x4F0026,
+	0x4E0003, 0x4D0000, 0x4C000C, 0x4B0C00, 0x4A0000, 0x49003F,
+	0x480001, 0x470081, 0x46C350, 0x450000, 0x4403E8, 0x430000,
+	0x4201F4, 0x410000, 0x401388, 0x3F0000, 0x3E0322, 0x3D00A8,
+	0x3C0000, 0x3B0001, 0x3A8001, 0x390020, 0x380000, 0x370000,
+	0x360000, 0x350000, 0x340820, 0x330080, 0x320000, 0x314180,
+	0x300300, 0x2F0300, 0x2E07FC, 0x2DC0DF, 0x2C1FA0, 0x2B0000,
+	0x2A0000, 0x290000, 0x280000, 0x270001, 0x260000, 0x250104,
+	0x240140, 0x230004, 0x220000, 0x211E21, 0x200393, 0x1F43ED,
+	0x1E318C, 0x1D318C, 0x1C0488, 0x1B0002, 0x1A0DB0, 0x190624,
+	0x18071A, 0x17007C, 0x160001, 0x150401, 0x14E048, 0x1327B7,
+	0x120064, 0x11012C, 0x100080, 0x0F064F, 0x0E1E40, 0x0D4000,
+	0x0C5001, 0x0B00A8, 0x0A10D8, 0x090604, 0x082000, 0x0740B2,
+	0x06C802, 0x0500C8, 0x040C43, 0x030642, 0x020500, 0x010809,
+	0x00241C, 0x00241C,
+};
+
 const u32 LMX2594G2500[LMX2594_COUNT] = 
 {
 	/* LMX2594_REF-250M__2500M.txt */
@@ -823,7 +848,7 @@ static bool XRFClk_SetConfigLMK()
 * @note         None
 *
 ****************************************************************************/
-static bool XRFClk_SetConfigLMX(u32 ChipID)
+static bool XRFClk_SetConfigLMX(u32 ChipID, u32 *arr, int count)
 {
 	int i;
 	
@@ -833,31 +858,6 @@ static bool XRFClk_SetConfigLMX(u32 ChipID)
 			return false;
 	}
 	return true;
-}
-
-/****************************************************************************/
-/**
-*
-* This function is used to set a full configuration on one of LMX2594 or
-* LMX04828 for the requested frequency.where the register settings is
-* provided from the selected hard coded data.
-*
-* @param        ChipId indicates the RF clock chip Id.
-* @param        ConfigId indicates the RF clock chip configuration Id.
-*
-* @return
-*       - XST_SUCCESS if successful.
-*       - XST_FAILURE if failed.
-*
-* @note         None
-*
-****************************************************************************/
-bool XRFClk_SetConfigOnOneChipFromConfigId(u32 ChipId)
-{
-	if (ChipId == RFCLK_LMK) 
-		return XRFClk_SetConfigLMK();
-	else
-		return XRFClk_SetConfigLMX(ChipId);
 }
 
 /****************************************************************************/
@@ -1112,10 +1112,13 @@ bool ConfigAdc()
 		return false;
 
 	/* Set config with ID */
-	if (!XRFClk_SetConfigOnOneChipFromConfigId(RFCLK_LMK))
+    if (!XRFClk_SetConfigLMK())
 		return false;
-	
-	if (!XRFClk_SetConfigOnOneChipFromConfigId(RFCLK_LMX2594_1)) 
+
+    if (!XRFClk_SetConfigLMX(RFCLK_LMX2594_1, LMX2594G2000, LMX2594_COUNT))
+		return false;
+
+    if (!XRFClk_SetConfigLMX(RFCLK_LMX2594_2, LMX2594G4000, LMX2594_COUNT))
 		return false;
 
 	return true;
